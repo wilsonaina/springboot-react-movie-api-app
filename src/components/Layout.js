54c9db6eaa8138539c5c963dfc820702992a460d
Wilson Aina <wilsonaina@hotmail.com>
2024-02-06 09:37:41 -0500

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import {Outlet} from "react-router-dom";
+import React from "react";
+
+export default function Layout () {
+
+    return(
+        <main>
+            <Outlet />
+        </main>
+    )
+}
